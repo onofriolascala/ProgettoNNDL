1437d31ec083c70990be435ceb18f4dc608e1822
onofrio <on.lascala@studenti.unina.it>
2026-02-26 17:53:07 +0100

    Implementato criterio di early stopping nel codice

--- shallow_network.py
+++ shallow_network.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -15,18 +17,32 @@ class Net(nn.Module):
         x = self.fc1(x)
         x = F.sigmoid(x)
         x = self.fc2(x)
-        x = F.sigmoid(x) #Da modificare con identità?
         return x
 
+
+def compute_k(epochs):
+    root = int(math.sqrt(epochs))
+    for k in range(root, 0, -1):
+        if epochs % k == 0:
+            return k
+    return root
+
+
+
 def train_net(train_loader, test_loader, net, device, optimizer, alpha, GL=True, loss_fn=nn.CrossEntropyLoss(), epochs=1000):
-    net.train()
+
     train_loss = []
     val_loss = []
+
     best_val_loss = np.inf
     best_train_loss = np.inf
     strip_train_loss = []
+    k = compute_k(epochs)
+    stop_criteria_value = 0
 
     for epoch in range(epochs):
+        net.train()
+        epoch_train_loss = 0.0
         for (data, labels) in train_loader:
             data, labels = data.to(device), labels.to(device)
 
@@ -38,29 +54,57 @@ def train_net(train_loader, test_loader, net, device, optimizer, alpha, GL=True,
             loss.backward()
             optimizer.step()
 
-            train_loss.append(loss.item())
+            epoch_train_loss += loss.item()
 
-            if loss.item() < best_train_loss:
-                best_train_loss = loss.item()
+        epoch_train_loss = epoch_train_loss / len(train_loader)
+        train_loss.append(epoch_train_loss)
 
-            if not GL:
-                strip_train_loss.append(loss.item())
+        if epoch_train_loss < best_train_loss:
+            best_train_loss = epoch_train_loss
+
+        if not GL:
+            strip_train_loss.append(epoch_train_loss)
 
 
-        for (data, labels) in test_loader:
+        net.eval()
+        epoch_val_loss = 0.0
+        with torch.no_grad():
 
-            data, labels = data.to(device), labels.to(device)
+            for (data, labels) in test_loader:
 
-            output = net(data)
+                data, labels = data.to(device), labels.to(device)
 
-            loss = loss_fn(output, labels)
+                output = net(data)
 
-            val_loss.append(loss.item())
+                loss = loss_fn(output, labels)
 
-            if loss.item() < best_val_loss:
-                best_val_loss = loss.item()
+                epoch_val_loss += loss.item()
 
-        if GL:
+
+            epoch_val_loss = epoch_val_loss / len(test_loader)
+
+            val_loss.append(epoch_val_loss)
+
+            if epoch_val_loss < best_val_loss:
+                best_val_loss = epoch_val_loss
+
+            gl_value = 100 * ((epoch_val_loss / best_val_loss) - 1)
+
+            if GL:
+                stop_criteria_value = gl_value
+            else:
+                if len(strip_train_loss) >= k:
+                    pk = 1000 * ((sum(strip_train_loss) / (k * best_train_loss)) - 1)
+                    stop_criteria_value = gl_value / pk
+                    #reset the strip
+                    strip_train_loss = []
+
+            if stop_criteria_value > alpha:
+                return train_loss, val_loss
+
+    return train_loss, val_loss
+
+
 
 
 
