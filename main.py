16cdf176d55ddb263494dca0402b98c247217d13
onofrio <on.lascala@studenti.unina.it>
2026-02-26 20:15:28 +0100

    aggiunto layout main

--- main.py
+++ main.py
@@ -4,104 +4,62 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader, random_split
-
-input_size = 28*28
-hidden_layer_size = 32
-output_size = 10
+import shallow_network as n, plot_util as plu, data_util as dtu
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        # First fully connected layer
-        self.fc1 = nn.Linear(input_size, hidden_layer_size)
-        self.fc2 = nn.Linear(hidden_layer_size, output_size)
-
-    def forward(self, x):
-        x = self.fc1(x) # equivale ad a
-        x = F.sigmoid(x) # g(a) = y
-        x = self.fc2(x)
-        x = F.sigmoid(x)
-        return x
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    input_size = 28 * 28
+    output_size = 10
+    max_epochs = 5000
+    batch_size = 10000
 
-    root = './data'  # Root directory where data will be stored
-    transform = transforms.ToTensor()  # Convert images to tensors
-    # Load the full dataset
-    full_dataset = datasets.MNIST(root=root, train=True, transform=transform, download=True)
-    full_testset = datasets.MNIST(root=root, train=False, transform=transform, download=True)
+    hidden_layer_size = [8, 16, 32, 64, 128]
+    alpha_GL = [0.5, 1, 1.5, 2, 2.5]                   # soglia di early stopping GL
+    alpha_PL = [0.5, 1, 1.5, 2, 2.5]                   # soglia di early stopping PL
 
-    generator = torch.Generator().manual_seed(42)
+    root = './data'                 # Root directory where data will be stored
+    plots = './plots'               # Directory dove verranno salvati i grafici
 
-    train_subset, _ = random_split(full_dataset, [10000, len(full_dataset)-10000], generator=generator)
-    test_subset, _ = random_split(full_testset, [2500, len(full_testset) - 2500], generator=generator)
+    gl_results = {}
+    pl_results = {}
+    std_results = {}
+    nets = {}
+    optimizers = {}
+    train_loader = dtu.get_train_loader(root, 10000, batch_size)
+    test_loader = dtu.get_test_loader(root, 5000, batch_size)
+    for index in hidden_layer_size:
+        nets[index] = n.Net(input_size, index, output_size)
+        nets[index].to(device) #questo serve a caricare le reti su gpu se disponibile
+        optimizers[index] = torch.optim.Rprop(nets[index].parameters(), lr=0.01)
 
-    print("[DEBUG] Train_subset len: ", len(train_subset))
-    print("[DEBUG] Test_subset len: ", len(test_subset))
-    # Print basic information about the dataset
-    print(f"Full dataset size: {len(full_dataset)} samples")
+    for index in hidden_layer_size:
+        # GL Early Stopping
+        for alpha in alpha_GL:
+            train_loss, val_loss, train_accuracy, val_accuracy, epochs = n.train_net(
+                train_loader, test_loader, nets[index], device, optimizers[index],
+                True, nn.CrossEntropyLoss(), alpha, True, max_epochs)
+            gl_results[index] = [train_loss, val_loss, train_accuracy, val_accuracy, epochs]
 
-    my_network = Net()
-    loss_criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Rprop(my_network.parameters(), lr=0.01)
+        # PL Early Stopping
+        for alpha in alpha_PL:
+            train_loss, val_loss, train_accuracy, val_accuracy, epochs = n.train_net(
+                train_loader, test_loader, nets[index], device, optimizers[index],
+                True, nn.CrossEntropyLoss(), alpha, False, max_epochs)
+            pl_results[index] = [train_loss, val_loss, train_accuracy, val_accuracy, epochs]
 
-    shuffle = True
-    batch_size = len(train_subset)
+        # Without Early Stopping
+        train_loss, val_loss, train_accuracy, val_accuracy, epochs = n.train_net(
+                train_loader, test_loader, nets[index], device, optimizers[index],
+                False, nn.CrossEntropyLoss(), 1, False, max_epochs)
+        std_results[index] = [train_loss, val_loss, train_accuracy, val_accuracy, epochs]
 
-    trainset_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=shuffle)
-
-    batch_size = len(test_subset)
-    validation_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=shuffle)
-
-    print(f"[DEBUG] Subset DataLoader: {len(trainset_loader)} batches per epoch")
-
-    my_network.train()
-
-    for epoch in range(100):
-
-        train_loss = 0.0
-
-        for batch_idx, (data, labels) in enumerate(trainset_loader):
-            # Flatten the images
-            data = data.view(-1, input_size)
-            #Transform the label in one hot coding
-            #labels_onehot = F.one_hot(labels, num_classes=10).float()
-            # Forward pass
-            outputs = my_network(data)
-            # loss = loss_criterion(outputs, labels_onehot)
-            loss = loss_criterion(outputs, labels)
-
-
-            # Backward pass and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-
-            if batch_idx % 10 == 0:
-                print(f'Batch {batch_idx}: Loss: {loss.item():.4f}')
-
-        my_network.eval()
-
-        valid_loss = 0.0
-        for data, labels in validation_loader:
-            data = data.view(-1, input_size)
-            labels_onehot = F.one_hot(labels, num_classes=10).float()
-            # Forward Pass
-            target = my_network(data)
-            # Find the Loss
-            loss = loss_criterion(target, labels_onehot)
-            # Calculate Loss
-            valid_loss += loss.item()
-
-        print(f'Epoch {epoch + 1} \t\t Training Loss: { \
-            train_loss / len(trainset_loader)} \t\t Validation Loss: { \
-            valid_loss / len(validation_loader)}')
+    key = input("Digitare 'plot' per salvare localmente i plot")
+    if 'plot' in key:
+        for index in hidden_layer_size:
+            plu.generic_plot()
 
     print(f"[DEBUG] Training complete")
 
