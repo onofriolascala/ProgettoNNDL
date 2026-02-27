b1a97b67483921a2bd5dfb701705191ef446acd2
onofrio <on.lascala@studenti.unina.it>
2026-02-27 14:42:08 +0100

    bug fixing e plot dei risultati, aggiunta classe per i dati dei train

--- main.py
+++ main.py
@@ -10,23 +10,23 @@ import shallow_network as n, plot_util as plu, data_util as dtu
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     input_size = 28 * 28
     output_size = 10
     max_epochs = 5000
-    batch_size = 10000
+    train_set_len= 10000
+    test_set_len= 5000
 
-    hidden_layer_size = [8, 16, 32, 64, 128]
-    alpha_GL = [0.5, 1, 1.5, 2, 2.5]                   # soglia di early stopping GL
-    alpha_PL = [0.5, 1, 1.5, 2, 2.5]                   # soglia di early stopping PL
+    hidden_layer_size = [8]#[8, 16, 32, 64, 128]
+    alpha_GL = [1]#[0.5, 1, 2, 3, 5]                   # soglia di early stopping GL
+    alpha_PL = [1]#[0.5, 0.75, 1, 2, 3]           # soglia di early stopping PL
 
     root = './data'                 # Root directory where data will be stored
     plots = './plots'               # Directory dove verranno salvati i grafici
 
-    gl_results = {}
-    pl_results = {}
-    std_results = {}
+    gl_results = []
+    pl_results = []
+    std_results = []
     nets = {}
     optimizers = {}
     train_loader = dtu.get_train_loader(root, 10000, batch_size)
@@ -37,29 +37,74 @@ if __name__ == '__main__':
         optimizers[index] = torch.optim.Rprop(nets[index].parameters(), lr=0.01)
 
     for index in hidden_layer_size:
+        nets[index] = n.Net(input_size, index, output_size)
+        nets[index].to(device)  # questo serve a caricare le reti su gpu se disponibile
+        optimizers[index] = torch.optim.Rprop(nets[index].parameters(), lr=0.01)
         # GL Early Stopping
         for alpha in alpha_GL:
-            train_loss, val_loss, train_accuracy, val_accuracy, epochs = n.train_net(
-                train_loader, test_loader, nets[index], device, optimizers[index],
-                True, nn.CrossEntropyLoss(), alpha, True, max_epochs)
-            gl_results[index] = [train_loss, val_loss, train_accuracy, val_accuracy, epochs]
-
+            train_info = n.get_training_info(train_loader, test_loader, nets[index], device, optimizers[index],
+                                            loss_fn = nn.CrossEntropyLoss(), early_stopping = True, alpha = alpha,
+                                            use_gl = True, epochs = max_epochs)
+            gl_results.append(train_info)
+        torch.cuda.empty_cache()
+        nets[index] = n.Net(input_size, index, output_size)
+        nets[index].to(device)  # questo serve a caricare le reti su gpu se disponibile
+        optimizers[index] = torch.optim.Rprop(nets[index].parameters(), lr=0.01)
         # PL Early Stopping
         for alpha in alpha_PL:
-            train_loss, val_loss, train_accuracy, val_accuracy, epochs = n.train_net(
-                train_loader, test_loader, nets[index], device, optimizers[index],
-                True, nn.CrossEntropyLoss(), alpha, False, max_epochs)
-            pl_results[index] = [train_loss, val_loss, train_accuracy, val_accuracy, epochs]
+            train_info = n.get_training_info(train_loader, test_loader, nets[index], device, optimizers[index],
+                                            loss_fn = nn.CrossEntropyLoss(), early_stopping = True, alpha = alpha,
+                                            use_gl = False, epochs = max_epochs)
+            pl_results.append(train_info)
 
+        torch.cuda.empty_cache()
+        nets[index] = n.Net(input_size, index, output_size)
+        nets[index].to(device)  # questo serve a caricare le reti su gpu se disponibile
+        optimizers[index] = torch.optim.Rprop(nets[index].parameters(), lr=0.01)
         # Without Early Stopping
-        train_loss, val_loss, train_accuracy, val_accuracy, epochs = n.train_net(
-                train_loader, test_loader, nets[index], device, optimizers[index],
-                False, nn.CrossEntropyLoss(), 1, False, max_epochs)
-        std_results[index] = [train_loss, val_loss, train_accuracy, val_accuracy, epochs]
+        train_info = n.get_training_info(train_loader, test_loader, nets[index], device, optimizers[index],
+                                         loss_fn = nn.CrossEntropyLoss(), early_stopping = False, epochs = max_epochs)
+        std_results.append(train_info)
 
-    key = input("Digitare 'plot' per salvare localmente i plot")
-    if 'plot' in key:
-        for index in hidden_layer_size:
-            plu.generic_plot()
+    key = input("Salvare localmente i plot? (y/n)")
+    if 'y' in key:
+        # GL Plotting
+        for train_info in gl_results:
+            # Plot Loss/Epochs
+            plu.generic_plot(f'GL Loss per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}', "Epochs", "Loss",
+                             f'{plots}/gl_loss_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
+                                [range(1,train_info.max_epoch+2), train_info.train_loss, "Training Loss"],
+                                         [range(1,train_info.max_epoch+2), train_info.val_loss, "Validation Loss"])
+            # Plot Accuracy/Epochs
+            plu.generic_plot(f'GL Accuracy per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}', "Epochs", "Accuracy",
+                             f'{plots}/gl_accuracy_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
+                                [range(1, train_info.max_epoch + 2), train_info.train_accuracy, "Training Accuracy"],
+                                         [range(1, train_info.max_epoch + 2), train_info.val_accuracy, "Validation Accuracy"])
+        # PL Plotting
+        for train_info in pl_results:
+            # Plot Loss/Epochs
+            plu.generic_plot(f'PL Loss per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}', "Epochs", "Loss",
+                             f'{plots}/pl_loss_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
+                                [range(1,train_info.max_epoch+2), train_info.train_loss, "Training Loss"],
+                                         [range(1,train_info.max_epoch+2), train_info.val_loss, "Validation Loss"])
+            # Plot Accuracy/Epochs
+            plu.generic_plot(f'PL Accuracy per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}', "Epochs", "Accuracy",
+                             f'{plots}/pl_accuracy_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
+                                [range(1, train_info.max_epoch + 2), train_info.train_accuracy, "Training Accuracy"],
+                                         [range(1, train_info.max_epoch + 2), train_info.val_accuracy, "Validation Accuracy"])
+        # NoES Plotting
+        for train_info in std_results:
+            # Plot Loss/Epochs
+            plu.generic_plot(f'NoES Loss per nodi interni={train_info.hidden_layer_size}', "Epochs", "Loss",
+                             f'{plots}/std_loss_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
+                                [range(1,train_info.max_epoch+2), train_info.train_loss, "Training Loss"],
+                                         [range(1,train_info.max_epoch+2), train_info.val_loss, "Validation Loss"])
+            # Plot Accuracy/Epochs
+            plu.generic_plot(f'NoES Accuracy per nodi interni={train_info.hidden_layer_size}', "Epochs", "Accuracy",
+                             f'{plots}/std_accuracy_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
+                                [range(1, train_info.max_epoch + 1), train_info.train_accuracy, "Training Accuracy"],
+                                         [range(1, train_info.max_epoch + 1), train_info.val_accuracy, "Validation Accuracy"])
 
     print(f"[DEBUG] Training complete")
+
+
--- shallow_network.py
+++ shallow_network.py
@@ -1,21 +1,22 @@
 import math
-
+from dataclasses import dataclass
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as f
 
 class Net(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(Net, self).__init__()
         # First fully connected layer
+        self.hidden_size = hidden_size
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         x = x.view(x.size(0), -1) #flatten dei dati
         x = self.fc1(x)
-        x = F.sigmoid(x)
+        x = f.sigmoid(x)
         x = self.fc2(x)
         return x
 
@@ -29,7 +30,7 @@ def compute_k(epochs):
 
 
 
-def train_net(train_loader, test_loader, net, device, optimizer, early_stopping=True, loss_fn=nn.CrossEntropyLoss(),alpha = 1, GL=True, epochs=5000):
+def train_net(train_loader, test_loader, net, device, optimizer, loss_fn=nn.CrossEntropyLoss(), early_stopping=True, alpha = 1, use_gl=True, epochs=5000):
 
     train_loss = []
     val_loss = []
@@ -38,15 +39,21 @@ def train_net(train_loader, test_loader, net, device, optimizer, early_stopping=
 
     best_val_loss = np.inf
     best_train_loss = np.inf
+    best_train_loss_epoch = 0
+    best_val_loss_epoch = 0
     strip_train_loss = []
     k = compute_k(epochs)
     stop_criteria_value = 0
+
+    print(f'[DEBUG] Starting training with number of nodes: {net.hidden_size} GL: {use_gl} alpha: {alpha} early_stopping: {early_stopping}')
 
     for epoch in range(epochs):
         net.train()
         epoch_train_loss = 0.0
         correct = 0
         total = 0
+        if epoch % 1000 == 0:
+            print("Starting epoch ", epoch)
         for (data, labels) in train_loader:
             data, labels = data.to(device), labels.to(device)
 
@@ -71,8 +78,9 @@ def train_net(train_loader, test_loader, net, device, optimizer, early_stopping=
 
         if epoch_train_loss < best_train_loss:
             best_train_loss = epoch_train_loss
+            best_train_loss_epoch = epoch
 
-        if not GL:
+        if not use_gl:
             strip_train_loss.append(epoch_train_loss)
 
 
@@ -104,11 +112,12 @@ def train_net(train_loader, test_loader, net, device, optimizer, early_stopping=
 
             if epoch_val_loss < best_val_loss:
                 best_val_loss = epoch_val_loss
+                best_val_loss_epoch = epoch
 
             gl_value = 100 * ((epoch_val_loss / best_val_loss) - 1)
 
             if early_stopping:
-                if GL:
+                if use_gl:
                     stop_criteria_value = gl_value
                 else:
                     if len(strip_train_loss) >= k:
@@ -119,11 +128,40 @@ def train_net(train_loader, test_loader, net, device, optimizer, early_stopping=
 
                 if stop_criteria_value > alpha:
                     #log training stopped at epoch: %epoch
-                    return train_loss, val_loss, train_accuracy, val_accuracy, epoch
+                    return train_loss, val_loss, train_accuracy, val_accuracy, best_train_loss_epoch, best_val_loss_epoch, epoch
             else:
                 strip_train_loss = []
 
-    return train_loss, val_loss, train_accuracy, val_accuracy, epochs
+    return train_loss, val_loss, train_accuracy, val_accuracy, best_train_loss_epoch, best_val_loss_epoch, epochs
+
+@dataclass
+class TrainInfo:
+    train_loss: list
+    val_loss: list
+    train_accuracy: list
+    val_accuracy: list
+    max_epoch: int
+    hidden_layer_size: int
+    alpha: float
+    best_train_loss_epoch: int
+    best_val_loss_epoch: int
+
+def get_training_info(train_loader, test_loader, net, device, optimizer, loss_fn=nn.CrossEntropyLoss(), early_stopping=True, alpha = 1, use_gl=True, epochs=5000):
+    train_loss, val_loss, train_accuracy, val_accuracy, best_train_loss_epoch, best_val_loss_epoch, max_epoch = train_net(train_loader, test_loader, net, device, optimizer, loss_fn, early_stopping, alpha, use_gl, epochs)
+    train_info = TrainInfo(
+        train_loss,
+        val_loss,
+        train_accuracy,
+        val_accuracy,
+        max_epoch,
+        net.hidden_size,
+        alpha,
+        best_train_loss_epoch,
+        best_val_loss_epoch,
+    )
+    return train_info
+
+
 
 
 
