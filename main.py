d723e8ca110aab0df1150043bd2e72ad75becc67
onofrio <on.lascala@studenti.unina.it>
2026-03-01 11:39:17 +0100

    Changes by Alfo

--- main.py
+++ main.py
@@ -14,93 +14,60 @@ print(f"Using {device} device")
 if __name__ == '__main__':
     input_size = 28 * 28
     output_size = 10
-    max_epochs = 5000
-    train_set_len= 10000
-    test_set_len= 5000
+    max_epochs = 5000  #5000
+    train_set_len = 10000
+    test_set_len = 5000
 
-    hidden_layer_size = [8]#[8, 16, 32, 64, 128]
-    alpha_GL = [1]#[0.5, 1, 2, 3, 5]                   # soglia di early stopping GL
-    alpha_PL = [1]#[0.5, 0.75, 1, 2, 3]           # soglia di early stopping PL
+    hidden_layer_size = [8, 16, 32, 64, 128]
+    alpha_GL = [0.5, 1, 2, 3, 5]                    # soglia di early stopping GL
+    alpha_PL = [0.5, 0.75, 1, 2, 3]                 # soglia di early stopping PL
 
-    root = './data'                 # Root directory where data will be stored
-    plots = './plots'               # Directory dove verranno salvati i grafici
+    root = './data'  # Root directory where data will be stored
+    plots = './plots'  # Directory dove verranno salvati i grafici
+    json_dir = './results_json'  # Directory per i json
 
     gl_results = []
     pl_results = []
-    std_results = []
+
     nets = {}
     optimizers = {}
     train_loader = dtu.get_train_loader(root, train_set_len, batch_size=train_set_len)
     test_loader = dtu.get_test_loader(root, test_set_len, batch_size=test_set_len)
 
-
     for index in hidden_layer_size:
+        # GL Early Stopping
         nets[index] = n.Net(input_size, index, output_size)
         nets[index].to(device)  # questo serve a caricare le reti su gpu se disponibile
         optimizers[index] = torch.optim.Rprop(nets[index].parameters(), lr=0.01)
-        # GL Early Stopping
         for alpha in alpha_GL:
             train_info = n.get_training_info(train_loader, test_loader, nets[index], device, optimizers[index],
                                              loss_fn=nn.CrossEntropyLoss(), early_stopping=True, alpha=alpha,
                                              use_gl=True, epochs=max_epochs)
             gl_results.append(train_info)
+        plu.trace_data(gl_results, "GL", plots)
+        jul.save_multiple_train_infos(gl_results, json_dir, "GL")
+
+        # PL Early Stopping
         torch.cuda.empty_cache()
         nets[index] = n.Net(input_size, index, output_size)
-        nets[index].to(device)  # questo serve a caricare le reti su gpu se disponibile
+        nets[index].to(device)
         optimizers[index] = torch.optim.Rprop(nets[index].parameters(), lr=0.01)
-        # PL Early Stopping
         for alpha in alpha_PL:
             train_info = n.get_training_info(train_loader, test_loader, nets[index], device, optimizers[index],
                                              loss_fn=nn.CrossEntropyLoss(), early_stopping=True, alpha=alpha,
                                              use_gl=False, epochs=max_epochs)
             pl_results.append(train_info)
+        plu.trace_data(pl_results, "PL", plots)
+        jul.save_multiple_train_infos(pl_results, json_dir, "PL")
 
+        # No Early Stopping
         torch.cuda.empty_cache()
         nets[index] = n.Net(input_size, index, output_size)
-        nets[index].to(device)  # questo serve a caricare le reti su gpu se disponibile
+        nets[index].to(device)
         optimizers[index] = torch.optim.Rprop(nets[index].parameters(), lr=0.01)
-        # Without Early Stopping
         train_info = n.get_training_info(train_loader, test_loader, nets[index], device, optimizers[index],
-                                         loss_fn = nn.CrossEntropyLoss(), early_stopping = False, epochs = max_epochs)
-        std_results.append(train_info)
-
-    key = input("Salvare localmente i plot? (y/n)")
-    if 'y' in key:
-        # GL Plotting
-        for train_info in gl_results:
-            # Plot Loss/Epochs
-            plu.generic_plot(f'GL Loss per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}', "Epochs", "Loss",
-                             f'{plots}/gl_loss_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
-                                [range(1,train_info.max_epoch+2), train_info.train_loss, "Training Loss"],
-                                         [range(1,train_info.max_epoch+2), train_info.val_loss, "Validation Loss"])
-            # Plot Accuracy/Epochs
-            plu.generic_plot(f'GL Accuracy per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}', "Epochs", "Accuracy",
-                             f'{plots}/gl_accuracy_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
-                                [range(1, train_info.max_epoch + 2), train_info.train_accuracy, "Training Accuracy"],
-                                         [range(1, train_info.max_epoch + 2), train_info.val_accuracy, "Validation Accuracy"])
-        # PL Plotting
-        for train_info in pl_results:
-            # Plot Loss/Epochs
-            plu.generic_plot(f'PL Loss per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}', "Epochs", "Loss",
-                             f'{plots}/pl_loss_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
-                                [range(1,train_info.max_epoch+2), train_info.train_loss, "Training Loss"],
-                                         [range(1,train_info.max_epoch+2), train_info.val_loss, "Validation Loss"])
-            # Plot Accuracy/Epochs
-            plu.generic_plot(f'PL Accuracy per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}', "Epochs", "Accuracy",
-                             f'{plots}/pl_accuracy_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
-                                [range(1, train_info.max_epoch + 2), train_info.train_accuracy, "Training Accuracy"],
-                                         [range(1, train_info.max_epoch + 2), train_info.val_accuracy, "Validation Accuracy"])
-        # NoES Plotting
-        for train_info in std_results:
-            # Plot Loss/Epochs
-            plu.generic_plot(f'NoES Loss per nodi interni={train_info.hidden_layer_size}', "Epochs", "Loss",
-                             f'{plots}/std_loss_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
-                                [range(1,train_info.max_epoch+2), train_info.train_loss, "Training Loss"],
-                                         [range(1,train_info.max_epoch+2), train_info.val_loss, "Validation Loss"])
-            # Plot Accuracy/Epochs
-            plu.generic_plot(f'NoES Accuracy per nodi interni={train_info.hidden_layer_size}', "Epochs", "Accuracy",
-                             f'{plots}/std_accuracy_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
-                                [range(1, train_info.max_epoch + 1), train_info.train_accuracy, "Training Accuracy"],
-                                         [range(1, train_info.max_epoch + 1), train_info.val_accuracy, "Validation Accuracy"])
+                                         loss_fn=nn.CrossEntropyLoss(), early_stopping=False, epochs=max_epochs)
+        plu.trace_data([train_info], "NoES", plots)
+        jul.save_multiple_train_infos([train_info], json_dir, "NoES")
 
     print(f"[DEBUG] Training complete")
--- plot_util.py
+++ plot_util.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-def generic_plot(title, xlabel, ylabel, path, *data_sets):
+def generic_plot(title, xlabel, ylabel, best_epoch, path, *data_sets):
     """
     Saves to system a jpg figure of the desired plot graph. The function takes an indefinite number of data
     sets and, if properly codified, draws the corresponding number of plots.
     :param title: title of figure
     :param xlabel: xlabel of figure
     :param ylabel: ylabel of figure
+    :param best_epoch: x-axis integer for best training epoch
+    :param path: file path where to save the figure
     :param data_sets: each data set is a tuple (x,y,label) where x are the x coordinates, y are the y coordinates and label is the data label for the plot
     """
     plt.figure()
@@ -15,68 +16,38 @@ def generic_plot(title, xlabel, ylabel, path, *data_sets):
 
     for data_set in data_sets:
         plt.plot(data_set[0], data_set[1], label = data_set[2])
-        legend.append(data_set[2])
-
-    #plt.axis((0, 125, 0, 330))
-
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-
-    plt.legend(legend)
-    plt.grid(linewidth=0.3)
-    #plt.show()
-    plt.savefig(path, bbox_inches='tight')
-    plt.close()
-
-def generic_plotv2(title, xlabel, ylabel, path, *data_sets):
-    """
-    Saves to system a jpg figure of the desired plot graph. The function takes an indefinite number of data
-    sets and, if properly codified, draws the corresponding number of plots.
-    :param title: title of figure
-    :param xlabel: xlabel of figure
-    :param ylabel: ylabel of figure
-    :param data_sets: each data set is a tuple (x,y,label) where x are the x coordinates, y are the y coordinates and label is the data label for the plot
-    """
-    plt.figure()
-    legend = []
-
-    for data_set in data_sets:
-        plt.plot(data_set[0], data_set[1], label = data_set[2])
-        if not data_set[2]:
+        if data_set[2] != "":
             legend.append(data_set[2])
 
-    #plt.axis((0, 125, 0, 330))
-
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
+    if best_epoch != 0:
+        plt.axvline(best_epoch, linewidth = 0.5, label = 'Best Epoch')
+        legend.append('Best Epoch')
     if len(legend)>0:
         plt.legend(legend)
 
-    plt.grid(linewidth=0.3)
-    plt.savefig(path, bbox_inches='tight')
+    plt.grid(linewidth = 0.3)
+    plt.savefig(path, bbox_inches = 'tight')
     plt.close()
 
 def trace_data(results, stopping_type, path):
     for train_info in results:
+        if stopping_type.find("NoES"):
+            title = f'{stopping_type} con nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}'
+        else:
+            title = f'{stopping_type} con nodi interni={train_info.hidden_layer_size}'
         # Plot Loss/Epochs
-        generic_plot(f'Loss {stopping_type} per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}',
-                         "Epochs", "Loss",
-                         f'{path}/{stopping_type}_loss_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
-                         [range(1, train_info.max_epoch + 2), train_info.train_loss, "Training Loss"],
-                         [range(1, train_info.max_epoch + 2), train_info.val_loss, "Validation Loss"])
+        generic_plot(f'Loss {title}',
+                         "Epochs", "Loss", train_info.best_val_loss_epoch,
+                         f'{path}/{stopping_type}/{stopping_type}_loss_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
+                         [range(1, len(train_info.train_loss)+1), train_info.train_loss, "Training Loss"],
+                                  [range(1, len(train_info.val_loss) + 1), train_info.val_loss, "Validation Loss"])
         # Plot Accuracy/Epochs
-        generic_plot(f'Accuracy {stopping_type} per nodi interni={train_info.hidden_layer_size} e alpha={train_info.alpha}',
-                         "Epochs", "Accuracy",
-                         f'{path}/gl_accuracy_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
-                         [range(1, train_info.max_epoch + 2), train_info.train_accuracy, "Training Accuracy"],
-                         [range(1, train_info.max_epoch + 2), train_info.val_accuracy, "Validation Accuracy"])
-
-def test_plot():
-    x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-    y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
-
-    temp_data = [x,y,"Temp"]
-    generic_plot("Title", "Xlabel", "Ylabel", temp_data)
+        generic_plot(f'Accuracy {title}',
+                         "Epochs", "Accuracy", train_info.best_val_loss_epoch,
+                         f'{path}/{stopping_type}/{stopping_type}_accuracy_{train_info.hidden_layer_size}-{train_info.alpha}.jpg',
+                         [range(1, len(train_info.train_accuracy)+1), train_info.train_accuracy, "Training Accuracy"], #train_info.max_epoch + 1
+                                  [range(1, len(train_info.val_accuracy) + 1), train_info.val_accuracy, "Validation Accuracy"])
